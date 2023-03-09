e5e8d68e18ac5412806f7f960363124a923e216c
Franco Liberali <franco.liberali@gmail.com>
2023-03-09 17:56:57 +0100

    add creating of training file

--- data_split.py
+++ data_split.py
@@ -131,10 +131,11 @@ TRAINING_SCENARIOS = [3, 4, 5, 7, 10, 11, 12, 13]
 TEST_SCENARIOS = [1, 2, 6, 8, 9]
 
 
-def concat_files(file_name, file_name_list):
+def create_training_file(file_name_list):
     full_training_file = pd.DataFrame()
 
     for file_name in file_name_list:
+        print(file_name)
         scenario = pd.read_csv(file_name, usecols=[
             'SrcAddr', 'DstAddr', 'Sport', 'Dport', 'Label'])
 
@@ -146,13 +147,13 @@ def concat_files(file_name, file_name_list):
             [full_training_file, scenario[['SrcAddr', 'DstAddr', 'Sport', 'Dport']]], ignore_index=True)
         del scenario
 
-    full_training_file.to_csv(file_name)
+    full_training_file.to_csv("training_file.binetflow")
     return full_training_file
 
 
 def data_split_2format():
-    concat_files("training_file.binetflow", [
-                 f"{file_number}.binetflow.2format" for file_number in TRAINING_SCENARIOS])
+    create_training_file(
+        [f"{file_number}.binetflow.2format" for file_number in TRAINING_SCENARIOS])
 
 
 data_split_2format()
